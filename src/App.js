6bd8970aba1e623c10b2569a82d56560220f3d35
booker <booker@booker.lan>
2020-12-29 22:13:04 +0200

    created more componenets

--- src/App.js
+++ src/App.js
@@ -4,6 +4,90 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
 
+ 
+const BlogList = ({blogs}) => {
+  return (
+    <div>
+      { blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+
+    </div>
+  )
+}
+const LoginForm = ({username , password, handleLogin, setUsername, setPassword}) => {
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+    </div>
+  )
+}
+
+const BlogForm = ({author,title,url,setAuthor,setTitle,setUrl, addBlog}) => {
+  return (
+    <div>
+        <form onSubmit={addBlog}>
+          <div>
+            title
+              <input
+              type="text"
+              value={title}
+              name="title"
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </div>
+          <div>
+            author
+              <input
+              type="text"
+              value={author}
+              name="author"
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+            </div>
+
+            <div>
+            url
+              <input
+              type="text"
+              value={url}
+              name="url"
+              onChange={({ target }) => setUrl(target.value)}
+            />  
+          </div>
+          <button type="submit">login</button>
+        </form>
+    </div>
+
+  )
+}
+
+const NavBar = ({username, logout}) => {
+  return (
+    <p> {username} logged in <button onClick={logout}> logout</button> </p>
+    )
+}
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
@@ -95,91 +179,43 @@ const App = () => {
 
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-          <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-          <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>      
-  )
 
   const logout = () => {
     localStorage.clear()
     setUser(null)
   }
 
-  const blogList = () => (
+  if (user === null) {
+   return( 
     <div>
-
-    <h2>blogs</h2>
-    {user.username} logged in 
-      <button onClick={logout}> logout</button>
-    {blogs.map(blog =>
-      <Blog key={blog.id} blog={blog} />
-    )}
-    
-    <form onSubmit={addBlog}>
-      <div>
-        title
-          <input
-          type="text"
-          value={title}
-          name="title"
-          onChange={({ target }) => setTitle(target.value)}
-        />
-      </div>
-      <div>
-        author
-          <input
-          type="text"
-          value={author}
-          name="author"
-          onChange={({ target }) => setAuthor(target.value)}
-        />
-        </div>
-
-        <div>
-        url
-          <input
-          type="text"
-          value={url}
-          name="url"
-          onChange={({ target }) => setUrl(target.value)}
-        />  
-      </div>
-      <button type="submit">login</button>
-    </form>
+      <Notification message={errorMessage} notificationType ={notificationType}/>
+      <LoginForm username={username} password={password} handleLogin={handleLogin} setUsername = {setUsername} setPassword ={setPassword} />
     </div>
-  )
+   )
+  } 
 
   return (
     <div>
-      <Notification message={errorMessage} notificationType ={notificationType}/>
+    <h2>blogs</h2>
+      <Notification 
+        message={errorMessage} 
+        notificationType ={notificationType}/>
 
-        {
-        user===null ?
-        loginForm():
-        blogList()
-        
-      }
-    
-  
+      <NavBar 
+        username={user.username} 
+        logout={logout} /> 
+
+      <BlogList 
+        blogs={blogs} 
+        logout={logout} />
+      <BlogForm 
+        author={author} 
+        title={title} 
+        url={url} 
+        setAuthor={setAuthor} 
+        setTitle={setTitle} 
+        setUrl={setUrl}
+        addBlog={addBlog} />
     </div>
   )
 }
